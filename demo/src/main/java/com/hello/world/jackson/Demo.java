49460685aba77dd7def49c8479431c2d27d1139c
xzh <x295132431@126.com>
2022-07-21 22:50:16 +0800

    init

---
+++ demo/src/main/java/com/hello/world/jackson/Demo.java
@@ -0,0 +1,7 @@
+package com.hello.world.jackson;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ flink-scala/src/main/scala/com/hello/world/scala/function/udaf/WeightedAvgAccumulator.java
@@ -0,0 +1,6 @@
+package com.hello.world.scala.function.udaf;
+
+public class WeightedAvgAccumulator {
+    public long sum = 0;
+    public int count = 0;
+}
